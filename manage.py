3ef49d945e6291b9c7ac4f2c50274a806295e252
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-16 15:50:27 -0400

    Add wsgi support

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
 from gevent import monkey
 monkey.patch_all()
+from geventwebsocket.server import WebSocketServer
 
 
 from presentation import app, socketio
 from flask.ext.script import Manager, Shell
+
+from werkzeug.contrib.fixers import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 manager = Manager(app)
 
@@ -16,7 +20,8 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 
 @manager.command
 def runserver():
-    socketio.run(app, "127.0.0.1", port=5001)
+    http_server = WebSocketServer(('127.0.0.1', 5001), app)
+    http_server.serve_forever()
 
 if __name__ == '__main__':
     manager.run()
