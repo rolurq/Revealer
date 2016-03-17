6fe799e668cabf8dd00e42472795cf4b9fa896da
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-17 00:30:36 -0400

    Store slideshow record on uploading

--- presentation/views.py
+++ presentation/views.py
@@ -1,7 +1,9 @@
 from flask import render_template, abort, request, flash, redirect, url_for
+from flask.ext.login import current_user
 from jinja2 import TemplateNotFound
 
-from . import app, slideshows
+from . import app, slideshows, db
+from .models import Slideshow
 
 multiplexer = {}
 multiplex_count = 0
@@ -19,7 +21,7 @@ def slide(presentation_name, user_type):
             multiplex_id = multiplex_count
             multiplexer[presentation_name] = multiplex_id
             multiplex_count += 1
-        return render_template("%s.html" % presentation_name,
+        return render_template(presentation_name,
                                user_type=user_type,
                                mult_id=multiplex_id)
     except TemplateNotFound:
@@ -30,6 +32,11 @@ def slide(presentation_name, user_type):
 def upload():
     if request.method == 'POST' and 'slideshow' in request.files:
         filename = slideshows.save(request.files['slideshow'])
+
+        record = Slideshow(title=filename, user=current_user)
+        db.session.add(record)
+        db.session.commit()
+
         flash("Slideshow saved.")
         return redirect(url_for('slide', presentation_name=filename,
                                 user_type='viewer'))
