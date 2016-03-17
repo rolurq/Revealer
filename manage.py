3f548541684d627b09bcf864ade7ad1276de002e
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-17 00:14:26 -0400

    Add users models

--- manage.py
+++ manage.py
@@ -2,14 +2,14 @@ from gevent import monkey
 monkey.patch_all()
 
 
-from presentation import app, socketio
+from presentation import app, socketio, db
 from flask.ext.script import Manager, Shell
 
 manager = Manager(app)
 
 
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=db)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
---
+++ presentation/models.py
@@ -0,0 +1,50 @@
+from flask.ext.login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from . import db
+from . import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Slideshow(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User',
+                           backref=db.backref('slideshows', lazy='dynamic'))
+
+    def __init__(self, title, user):
+        self.title = title
+        self.user = user
+
+    def __repr__(self):
+        return '<Slideshow %r>' % self.title
