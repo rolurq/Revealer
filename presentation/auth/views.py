cd4cdfd6bdc37960daeea53967ddea0f8ab20493
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-18 03:15:52 -0400

    Add registration route

--- presentation/auth/views.py
+++ presentation/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask.ext.login import login_user, login_required, logout_user
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login/', methods=['GET', 'POST'])
@@ -25,3 +26,14 @@ def logout():
     logout_user()
     flash("You have been logged out", category='info')
     return redirect(url_for('index'))
+
+
+@auth.route('/register/', methods=('GET', 'POST'))
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('auth/register.html', form=form)
