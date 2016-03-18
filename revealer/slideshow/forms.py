adae33fae79b3009c0a71a4cc62a7b38ebabd68c
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-18 06:30:53 -0400

    Add validator to control repeated slideshow titles

--- revealer/slideshow/forms.py
+++ revealer/slideshow/forms.py
@@ -1,8 +1,11 @@
 from flask.ext.wtf import Form
 from flask.ext.wtf.file import FileRequired, FileAllowed, FileField
+from flask.ext.login import current_user
 from wtforms.fields import StringField, SubmitField
 from wtforms.validators import Required
+from wtforms import ValidationError
 from .. import slideshows
+from ..models import Slideshow
 
 
 class SlideshowForm(Form):
@@ -11,3 +14,8 @@ class SlideshowForm(Form):
                                              FileAllowed(slideshows,
                                                          'HTML files only!')])
     submit = SubmitField('Upload')
+
+    def validate_title(self, field):
+        if Slideshow.query.filter_by(title=field.data,
+                                     user=current_user).first():
+            raise ValidationError('Slideshow title already exists')
