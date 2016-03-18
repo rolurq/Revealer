7f07749ba521211952ba42c34716803ec0fb9b8c
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-18 02:33:29 -0400

    Add custom 404 error page

--- presentation/views.py
+++ presentation/views.py
@@ -5,3 +5,8 @@ from . import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
