f73c3ce358269a60399128631b1e001b11483245
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-17 22:31:00 -0400

    Add migration support

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ monkey.patch_all()
 
 from presentation import app, socketio, db
 from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
 
 manager = Manager(app)
 
@@ -12,6 +13,8 @@ def make_shell_context():
     return dict(app=app, db=db)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 @manager.command
