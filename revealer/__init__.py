deba94af21c65f5a421c39284af1ab8613a7fc57
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-18 17:15:29 -0400

    Add category to login message

--- revealer/__init__.py
+++ revealer/__init__.py
@@ -17,6 +17,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'success'
 
 db = SQLAlchemy()
 db.init_app(app)
