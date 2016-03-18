79bcdcaa4a304ff4ba8c29ec2ae206fe40953059
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-18 04:00:26 -0400

    Fix missing commiting after registration
    In order to use confirmation email, this line should be removed

--- presentation/auth/views.py
+++ presentation/auth/views.py
@@ -34,6 +34,8 @@ def register():
     if form.validate_on_submit():
         user = User(username=form.username.data, password=form.password.data)
         db.session.add(user)
+        # remove for comfirmation sending
+        db.session.commit()
         login_user(user)
         return redirect(url_for('index'))
     return render_template('auth/register.html', form=form)
