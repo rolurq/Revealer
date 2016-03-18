65262d71b8beedb895f0770cd42b5fae6c858246
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-18 02:36:53 -0400

    Add logout route

--- presentation/auth/views.py
+++ presentation/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user
+from flask.ext.login import login_user, login_required, logout_user
 from . import auth
 from ..models import User
 from .forms import LoginForm
@@ -17,3 +17,10 @@ def login():
         flash('Invalid username or password', category='warning')
 
     return render_template('auth/login.html', form=form)
+
+@auth.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out", category='info')
+    return redirect(url_for('index'))
