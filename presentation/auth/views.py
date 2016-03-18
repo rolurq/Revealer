63859618113dcf1b1d0250684caf34de305e329b
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-18 02:00:40 -0400

    Add category to flash message

--- presentation/auth/views.py
+++ presentation/auth/views.py
@@ -5,13 +5,15 @@ from ..models import User
 from .forms import LoginForm
 
 
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/login/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('index'))
-        flash('Invalid username or password')
+        flash('Invalid username or password', category='warning')
+
     return render_template('auth/login.html', form=form)
