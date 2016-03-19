d0e9bf7fd32969af3bc15f5cb81020dd68c2b520
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-19 19:18:38 -0400

    Add error handler for 401 code

--- revealer/views.py
+++ revealer/views.py
@@ -10,3 +10,8 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
+
+@app.errorhandler(401)
+def unauthorized(e):
+    return render_template("401.html"), 401
