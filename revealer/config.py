5db0147858aa502c753933402049b7d8b0ad2f47
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-19 19:22:59 -0400

    Use mysql

--- revealer/config.py
+++ revealer/config.py
@@ -2,10 +2,14 @@ import os
 basedir = os.path.dirname(__file__)
 abs_basedir = os.path.abspath(basedir)
 
+MYSQL_USER = "revealer"
+MYSQL_PASSWD = "revealer"
+MYSQL_SERVER = "localhost:3306"
+DATABASE_NAME = "data"
 DEBUG = 'true'
 UPLOADED_SLIDESHOWS_DEST = os.path.join(basedir, 'templates', 'slideshows')
 SECRET_KEY = 's0m3 53cr3t k3y'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % os.path.join(abs_basedir, 'data',
-                                                        'data.sqlite')
+SQLALCHEMY_DATABASE_URI = 'mysql://%s:%s@%s/%s' % (MYSQL_USER, MYSQL_PASSWD,
+                                                   MYSQL_SERVER, DATABASE_NAME)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
