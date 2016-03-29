5fdca6b92de7f5f8720e517ef270bcbea2994ae6
Rolando Urquiza <rolurquiza@gmail.com>
2016-03-29 00:28:49 +0000

    Setup models

--- revealer/admin/models.py
+++ revealer/admin/models.py
@@ -1,9 +1,20 @@
 from flask.ext.admin.contrib import sqla
+from flask.ext.login import current_user
 
 
-class UserAdmin(sqla.ModelView):
-    pass
+class RevealerModelView(sqla.ModelView):
+
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.id == 1
 
 
-class SlideshowAdmin(sqla.ModelView):
-    pass
+class UserAdmin(RevealerModelView):
+    column_exclude_list = ('password_hash', )
+    form_excluded_columns = column_exclude_list
+
+
+class SlideshowAdmin(RevealerModelView):
+    can_create = False
+    column_exclude_list = ('presentations', 'created', 'last_presented')
+    
+    form_excluded_columns = column_exclude_list
